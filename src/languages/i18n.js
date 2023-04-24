237ae0ed0f1a92810f08fb55090a82a0972e5445
Luke Wang <luke@ubersuggest.com>
2023-04-24 16:50:47 -0400

    - Add languages

--- src/languages/i18n.js
+++ src/languages/i18n.js
@@ -1,14 +1,46 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+import de from './de.json';
 import en from './en.json';
+import es from './es.json';
+import fr from './fr.json';
+import it from './it.json';
+import ja from './ja.json';
+import nl from './nl.json';
+import pt from './pt.json';
+import zh from './zh.json';
 
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
+  de: {
+    translation: de,
+  },
   en: {
     translation: en,
+  },
+  es: {
+    translation: es,
+  },
+  fr: {
+    translation: fr,
+  },
+  it: {
+    translation: it,
+  },
+  ja: {
+    translation: ja,
+  },
+  nl: {
+    translation: nl,
+  },
+  pt: {
+    translation: pt,
+  },
+  zh: {
+    translation: zh,
   },
 };
 
