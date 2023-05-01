b0e671e384290161f7d97ec91188b39739879e36
Luke Wang <luke@ubersuggest.com>
2023-05-01 15:31:36 -0400

    - Integrate Redux toolkit

--- src/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const TIMESTAMP_LIFETIME = 5 * 60; // timestamp expires in 5 minutes
---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { rootReducer } from 'store/reducers';
+import { aiWriterApi } from './ai-writer/ai-writer.api';
+
+export const store = configureStore({
+  reducer: {
+    ...rootReducer,
+    [aiWriterApi.reducerPath]: aiWriterApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(aiWriterApi.middleware),
+});
+
+setupListeners(store.dispatch);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
